0395b9fc3d8dcf3d2dc5665baf97554011fc0a79
RuntimeRacer <sge2022@gmail.com>
2023-11-16 18:33:13 +0100

    Revert "Test using "insanely fast whisper" for cloud transcription"
    This reverts commit 3dba9548393ba8072e5a58843ef7180a57bd3fa6.

--- diarize_worker.py
+++ diarize_worker.py
@@ -281,7 +281,6 @@ class DiarizeWorker:
             audio,
             chunk_length_s=30,
             batch_size=self.batch_size,
-            generate_kwargs={"task": 'transcribe'},
             return_timestamps="word"
         )
         word_timestamps = []
