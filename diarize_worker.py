d1214298e742dd7f55fb0726b17c7e9814f373e1
RuntimeRacer <sge2022@gmail.com>
2023-11-18 01:33:15 +0100

    fix cache not fully utilized because of prefetch-size being same as cache size.

--- diarize_worker.py
+++ diarize_worker.py
@@ -93,7 +93,7 @@ class DiarizeWorker:
                     heartbeat=1800
                 ))
                 self.polling_channel_ref = self.polling_connection.channel()
-                self.polling_channel_ref.basic_qos(prefetch_count=self.cache_size)
+                self.polling_channel_ref.basic_qos(prefetch_count=self.cache_size+1)
                 self.polling_channel_ref.queue_declare(queue=self.poll_channel, durable=True)
                 self.connection_active = True
                 logging.info("Polling Worker: Successfully connected to RabbitMQ host")
